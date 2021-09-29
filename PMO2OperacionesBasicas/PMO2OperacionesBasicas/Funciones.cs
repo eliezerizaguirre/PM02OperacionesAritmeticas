9ab8b6206454e25b79eb1d7964663c785742e322
eliezerizaguirre <eliezer65izaguirre@gmail.com>
2021-09-28 23:41:01 -0600

    Agregar archivos de proyecto.

---
+++ PMO2OperacionesBasicas/PMO2OperacionesBasicas/Funciones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PMO2OperacionesBasicas
+{
+    public class Funciones
+    {
+        public Funciones()
+        {
+        }
+        
+        public void suma(double num1, double num2)
+        {
+            double suma = num1 + num2;
+            
+        }
+
+        public void resta(double num1, double num2)
+        {
+            double resta = num1 - num2;
+        }
+
+        public void multiplicacion(double num1, double num2)
+        {
+            double multiplicacion = num1 * num2;
+        }
+
+        public void division(double num1, double num2)
+        {
+            double division = num1 / num2;
+        }
+
+    }
+
+}
---
+++ PMO2OperacionesBasicas/PMO2OperacionesBasicas/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PMO2OperacionesBasicas
+{
+    public partial class MainPage : ContentPage
+    {
+        Funciones obj = new Funciones();
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnsumar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+            int suma;
+            //txtresult.Text = int.Parse(obj.suma((double)num1, (double)num2)).ToString();
+            txtresult.Text = (double.Parse(txtnum2.Text) + int.Parse(txtnum1.Text)).ToString();
+            await Navigation.PushAsync(new SecondPage());
+        }
+
+        private async void btnrestar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+            //obj.resta((double)num1, (double)num2);
+            txtresult.Text = (double.Parse(txtnum2.Text) - int.Parse(txtnum1.Text)).ToString();
+            await Navigation.PushAsync(new SecondPage());
+        }
+
+        private async void btnmultiplicar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+            //obj.multiplicacion((double)num1, (double)num2);
+            txtresult.Text = (double.Parse(txtnum2.Text) * int.Parse(txtnum1.Text)).ToString();
+            await Navigation.PushAsync(new SecondPage());
+        }
+
+        private async void btndividir_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtnum1.Text);
+            var num2 = Convert.ToDecimal(txtnum2.Text);
+            //obj.division((double)num1, (double)num2);
+            txtresult.Text = (double.Parse(txtnum2.Text) / int.Parse(txtnum1.Text)).ToString();
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
